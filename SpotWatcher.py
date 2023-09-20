f22e543a3e46c30ac6e10c8682925164e13701f4
tylerwight <tylermwight@gmail.com>
2023-09-19 20:00:09 -0600

    update guild data formatting, reduce loops

--- SpotWatcher.py
+++ SpotWatcher.py
@@ -166,49 +166,68 @@ async def on_ready():
                     print("Guild already exists, loading it's information")
                     cursor.execute("SELECT * FROM guilds where guild_id=%s",([y]))
                     records = cursor.fetchall()
-                    #print(f"records is {records}")
-                    guild_data.append(records)
+
+                    listed_records = [list(row) for row in records]
+                    print(f"records is {records}")
+                    print(f"listed_records is {listed_records}")
+                    guild_data.append(listed_records[0])
 
 
                     #print(row)
 
         if (duplicate == 0):
+            print("New Guild detected, adding to DB")
             sql = "INSERT INTO guilds (guild_id,name,enabled) VALUES (%s, %s, %s)"
             val = (str(guild.id),str(guild), 0)
             cursor.execute(sql, val)
             mydb.commit()
             print(f'I inserted {cursor.rowcount} rows using {guild.id} and {guild}')
+            print("adding to guild_data")
+            item = [str(guild.id), str(guild), None, None, 0, None, None]
+            guild_data.append(item)
+        
+        if (duplicate > 1):
+            print("more than one duplicate!? something weird is going on.")
 
     print("")
-    print(f"guild_data loaded from DB is {guild_data}")
+    print(f"guild_data is {guild_data}")
+    print(f"guild_data is type: {type(guild_data)}")
+    for item in guild_data:
+        print(f"guild data item: {item}")
+        print(f"item type: {type(item)}")
+            
 
 
 
 
 @bot.event
 async def on_message(message):
-    #spottoken = util.prompt_for_user_token(username, scope, client_id=cid, client_secret=secret, redirect_uri="http://localhost:8080")
-    #spotify = spotipy.Spotify(auth=spottoken)
-    
     #ignore messages from the bot
-    if str(message.author.id) == str(bot.user_id):
+    if str(message.author.id) == str(bot.user.id):
         return
+    
+    id = message.guild.id
+    for i in guild_data:
+        if (int(i[0]) == id):
+            current_guild = i
+
+    print(f"printing current_guild : {current_guild}")
+    
 
     channel = str(message.channel.id)
     channel_name = bot.get_channel(message.channel.id)
     textSearch = "spotify.com/track"
     textSearch2 = "spotify.com/album"
 
-
-    if bot.watch != 1:
-        if logging == 1:
-            print("Not in watch mode, skipping...")
+    if current_guild[4] != 1:
+        print("Guild has not eanbled bot, skipping...")
         await bot.process_commands(message)
         return
 
-    if channel != bot.watchchannel:
+
+    if channel != current_guild[3]:
         if logging == 1:
-            print("Not an enabled channel, skipping...")
+            print("Not an enabled channel for this guild, skipping...")
         await bot.process_commands(message)
         return
 
@@ -251,19 +270,49 @@ async def on_message(message):
 
 
 @bot.command(brief='start watching specified channel')
-async def watch_channel(ctx):
-    if bot.watch == 0:
-        bot.watch = 1
-        await ctx.channel.send("I am now watching the text channel specified in code for spotify links")
+async def enable(ctx):
+    id = ctx.message.guild.id
+    for index,i in enumerate(guild_data):
+        if (int(i[0]) == id):
+            current_guild = i
+            guild_index = index
+
+
+    if (guild_data[guild_index][4] == 0):
+        guild_data[guild_index][4] = 1
+        await ctx.channel.send(f"Enabled and monitoring for guild {guild_data[guild_index][1]}")
     else:
-        bot.watch = 0
-        await ctx.channel.send("I am no longer watching a text channel")
+        await ctx.channel.send(f"Disabled and not monitoring for guild {guild_data[guild_index][1]}")
+        guild_data[guild_index][4] = 0
+
+    sql = "UPDATE guilds set enabled = %s where guild_id = %s"
+    val = (guild_data[guild_index][4], guild_data[guild_index][0])
+    cursor.execute(sql, val)
+    mydb.commit()
+
 
 @bot.command(brief='show playlist currently being added to')
 async def get_playlist(ctx):
     convert="spotify:playlist:" + bot.playlist_id
     output_link=URIconverter(convert)
     await ctx.channel.send(" I am currently adding songs to this playlist: " + output_link)
+
+
+@bot.command(brief='Set which text channel to watch for spotify links')
+async def set_channel(ctx, *, channel_id):
+    id = ctx.message.guild.id
+    for index,i in enumerate(guild_data):
+        if (int(i[0]) == id):
+            current_guild = i
+            guild_index = index
+    
+    print(f"trying to set guild: {current_guild} channel id to {channel_id}")
+    guild_data[guild_index][3] = channel_id
+    sql = "UPDATE guilds set watch_channel = %s where guild_id = %s"
+    val = (channel_id, current_guild[0])
+    cursor.execute(sql, val)
+    mydb.commit()
+
 
 
 @bot.command(brief='Choose the playlist to add to. Will create a new playlist if none exists or attach to an existing with the same name')
