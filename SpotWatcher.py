70802083c078e6af10ddf793c6cfe8f9dd5a8d6c
tylerwight <tylermwight@gmail.com>
2023-09-20 19:27:30 -0600

    add on_guild_join

--- SpotWatcher.py
+++ SpotWatcher.py
@@ -217,8 +217,54 @@ async def on_ready():
 
     print(f"guild_data loaded is {guild_data}")
 
+@bot.event
+async def on_guild_join(guild):
+    print("===========================")
+    print(f'{guild} has  just added the discord bot!!')
+    print("===========================")
 
-        
+
+    duplicate = 0
+    cursor.execute("SELECT guild_id FROM guilds")
+    existing_guilds = cursor.fetchall()
+
+
+    for x in existing_guilds:
+        for y in x:
+            if str(guild.id) in y:
+                duplicate = duplicate + 1
+                if logging == 1:
+                    print("duplicate guild id found!")
+                    print("Guild already exists, loading it's information from DB")
+                    
+                #get data from DB for existing guild
+                cursor.execute("SELECT * FROM guilds where guild_id=%s",([y]))
+                records = cursor.fetchall()
+                #convert to list because it's a list of tuples by default
+                listed_records = [list(row) for row in records]
+                if logging == 1:
+                    print(f"data loaded for guild: {listed_records[0]}")
+
+                guild_data.append(listed_records[0])
+
+    #Guild does not exist in DB, create it in the DB
+    if (duplicate == 0):
+        print("New Guild detected, adding to DB")
+        sql = "INSERT INTO guilds (guild_id,name,enabled) VALUES (%s, %s, %s)"
+        val = (str(guild.id),str(guild), 0)
+        cursor.execute(sql, val)
+        mydb.commit()
+        print(f'I inserted {cursor.rowcount} rows using {guild.id} and {guild}')
+        print("adding to guild_data")
+        item = [str(guild.id), str(guild), None, None, 0, None, None]
+        guild_data.append(item)
+    
+    if (duplicate > 1):
+        print("more than one duplicate!? something weird is going on.")
+
+
+    print("===========================")
+    print("===========================")
 
 
 #=============
