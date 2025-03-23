92c37a68009d290e71fa7e888d5df0c56d29931f
Tyler <tylermwight@gmail.com>
2025-03-23 05:36:59 +0000

    more convert guild_data list to guilds_state Dict

--- bot/bot_core.py
+++ bot/bot_core.py
@@ -19,59 +19,70 @@ class CommunityPlaylistBot(commands.Bot):
         self.sqlpass = kwargs.pop('sqlpass', None)
         self.spotify_cid = kwargs.pop('spotify_cid', None)
         self.spotify_secret = kwargs.pop('spotify_secret', None)
-
-
-        #self.guild_data = [] #State of the guilds in memory {guild_id, name, Spotipy_username, watch_channel, enabled, playlist_name, playlist_id}
-        self.guilds_state = {} # global state of all guilds (so we don't have to pull from mysql every time we need data)
-        self.idx_guild_id = 0 #index id of guild_data to make more readable (probably should just use a dict)
-        self.idx_guild_name = 1
-        self.idx_spot_username = 2
-        self.idx_watch_channel = 3
-        self.idx_enabled = 4
-        self.idx_playlist_name = 5
-        self.idx_playlist_id = 6
-
         self.spotify_scope = 'playlist-modify-public'
+
+        self.guilds_state = {} # global state of all guilds (so we don't have to pull from mysql every time we need data)
+
+       
 
 
     async def get_guild_data(self, guild_id):
-        for guild in self.guild_data:
-            if guild[self.idx_guild_id] == guild_id:
-                logging.info(f"Found global guild data for:{guild_id}\n\t returning: {guild}")
-                return guild
-        logging.warning(f"couldn't find global guild data for {guild_id}")
-        return None
+        guild_id_str = str(guild_id)
+        guild_data = self.bot.guilds_state.get(guild_id_str)
+        if guild_data is None:
+            logging.warning(f"Couldn't find global guild data for {guild_id_str}")
+            return None
 
+        logging.info(f"Found global guild data for: {guild_id_str}\n\t returning: {guild_data}")
+        return guild_data
 
     async def update_guild(self, guild_id):
-        logging.info(f"Reading from DB and updating internal state")
-        guild = self.get_guild(guild_id)
-        if guild is None: return None
-
+        logging.info(f"Reading from DB and updating internal state for guild_id {guild_id}")
+        guild_obj = self.get_guild(guild_id)
+        logging.info(f"found guild_obj: {guild_obj}")
+        if guild_obj is None:
+            logging.error(f"trying to update guild {guild_id}, but couldn't find guild connected to bot")
+            return None
+        
         try:
-            mydb = mysql.connector.connect(host = "localhost", user = self.sqluser, password = self.sqlpass, database = "discord")
+            mydb = mysql.connector.connect(
+                host="localhost",
+                user=self.sqluser,
+                password=self.sqlpass,
+                database="discord"
+            )
             cursor = mydb.cursor()
         except Exception as e:
-            logging.error(f'Error connecting to Mysql DB error: {e}')
+            logging.error(f"Error connecting to MySQL DB: {e}")
+            return None
 
+        guild_id_str = str(guild_id)
 
-        for x in self.guild_data:
-            if x[self.idx_guild_id] == str(guild.id):
-                cursor.execute("SELECT * FROM guilds where guild_id=%s",([x[0]]))
-                record = cursor.fetchall()
+        try:
+            cursor.execute("SELECT * FROM guilds WHERE guild_id=%s", (guild_id_str,))
+            record = cursor.fetchone()
+            if not record:
+                logging.warning(f"No DB record found for guild_id {guild_id_str}")
+                return None
+            
+            #read data from DB into memory
+            self.guilds_state[guild_id_str] = {
+                "guild_id":         record[0],
+                "name":             record[1],
+                "spotipy_username": record[2],
+                "watch_channel":    record[3],
+                "enabled":          record[4],
+                "playlist_name":    record[5],
+                "playlist_id":      record[6],
+            }
 
-                logging.debug(f"Trying to update from DB: updating x[0] {x} with this data: {record[0]}")
+            logging.info(f"Updated guild data in memory: {self.guilds_state[guild_id_str]}")
 
-                x[self.idx_guild_name]    = record[0][self.idx_guild_name]
-                x[self.idx_spot_username] = record[0][self.idx_spot_username]
-                x[self.idx_watch_channel] = record[0][self.idx_watch_channel]
-                x[self.idx_enabled]       = record[0][self.idx_enabled]
-                x[self.idx_playlist_name] = record[0][self.idx_playlist_name]
-                x[self.idx_playlist_id]   = record[0][self.idx_playlist_id]
-                print(x)
-        
-        cursor.close()
-        mydb.close()
+        except Exception as e:
+            logging.error(f"Error during SELECT or dict update: {e}")
+        finally:
+            cursor.close()
+            mydb.close()
 
         return None
 
@@ -93,13 +104,14 @@ class CommunityPlaylistBot(commands.Bot):
             logging.warning(f"commit_to_db: Guild data not found for {guild_id}")
             return
 
-        column_index_map = {
-            "name": self.idx_guild_name,
-            "spotipy_username": self.idx_spot_username,
-            "watch_channel": self.idx_watch_channel,
-            "enabled": self.idx_enabled,
-            "playlist_name": self.idx_playlist_name,
-            "playlist_id": self.idx_playlist_id
+
+        db_col_map = {
+            "name": "name",
+            "spotipy_username": "spotipy_username",
+            "watch_channel": "watch_channel",
+            "enabled": "enabled",
+            "playlist_name": "playlist_name",
+            "playlist_id": "playlist_id"
         }
 
         try:
@@ -111,22 +123,23 @@ class CommunityPlaylistBot(commands.Bot):
             )
             cursor = mydb.cursor()
 
-            set_clause = ", ".join(f"{col} = %s" for col in columns)
+            set_clause = ", ".join(f"{db_col_map[col]} = %s" for col in columns)
             values = []
 
             for col in columns:
-                if col in column_index_map:
-                    values.append(current_guild[column_index_map[col]])
-                else:
-                    logging.warning(f"commit_to_db: Unknown column '{col}' requested")
+                if col not in db_col_map:
+                    logging.warning(f"commit_to_db: Unknown column '{col}' requested.")
                     return
 
-            values.append(guild_id)
+                values.append(current_guild[col])
+
+            values.append(str(guild_id))
 
             sql = f"UPDATE guilds SET {set_clause} WHERE guild_id = %s"
+
             logging.info(f"commit_to_db: executing {sql}")
             logging.info(f"\twith values: {values}")
-            
+
             cursor.execute(sql, values)
             mydb.commit()
 
